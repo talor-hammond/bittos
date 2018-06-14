b3fdf26374206e69202eaf12207ba508d80cce41
stanley-roache <stan.roache@gmail.com>
2018-06-15 11:48:07 +1200

    wrote db functions for everything, did some shit

--- db.js
+++ db.js
@@ -1,5 +1,54 @@
 const knex = require('knex')
 const config = require('./knexfile').development
 
-const db = knex(config)
+const conn = knex(config)
 
+function grabUser(id, testConn) {
+  const db = testConn || conn
+
+  return db('users')
+    .where({id})
+    .first()
+}
+
+function grabPhotosOfUser(id, testConn){
+  const db = testConn || conn
+
+  return db('images')
+    .where({artist_id: id})
+    .orderBy('created_at', 'asc')
+}
+
+function createNewUser(newUser, testConn){
+  const db = testConn || conn
+
+  return db('users')
+    .insert(newUser)
+}
+
+function getGallery(testConn) {
+  const db = testConn || conn
+
+  return db('images')
+}
+
+function getImage(id, testConn) {
+  const db = testConn || conn
+
+  return db('images')
+    .where({id})
+    .first()
+}
+
+module.exports = {
+    grabUser,
+    grabPhotosOfUser,
+    createNewUser,
+    getGallery,
+    getImage
+}
+
+// testing calls
+// getImage(1).then(res => {
+//   console.log(res);
+// })
--- seeds/users.js
+++ seeds/users.js
@@ -5,10 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, name: 'Stanley Roache', username: 'theroacheapproach69', avatar_url: 'https://cps-static.rovicorp.com/3/JPG_500/MI0002/422/MI0002422352.jpg?partner=allrovi.com'},
-        {id: 2, name: 'Phoenix Stryker', username: 'flamecatcher34', avatar_url: 'https://skateparkoftampa.com/spot/headshots/5572.jpg'},
-        {id: 3, name: 'Rebecca Dugdale', username: 'dugrebby1000', avatar_url: 'https://media.gettyimages.com/photos/man-upside-down-in-dirt-hole-with-legs-in-the-air-picture-id108000160'},
-        {id: 4, name: 'Tay', username: 'rickybobby31', avatar_url: 'https://pbs.twimg.com/profile_images/1264265275/Ricky_Bobby_closeup_400x400.jpg'}
+        {id: 1, name: 'Stanley Roache', username: 'theroacheapproach69', email: 'stan@stan.com', bio: 'Really tall', avatar_url: 'https://cps-static.rovicorp.com/3/JPG_500/MI0002/422/MI0002422352.jpg?partner=allrovi.com'},
+        {id: 2, name: 'Phoenix Stryker', username: 'flamecatcher34', email: 'phoenix@phoenix.com', bio: 'Likes apples', avatar_url: 'https://skateparkoftampa.com/spot/headshots/5572.jpg'},
+        {id: 3, name: 'Rebecca Dugdale', username: 'dugrebby1000', email: 'reb@reb.com', bio: 'Accidental shade all day', avatar_url: 'https://media.gettyimages.com/photos/man-upside-down-in-dirt-hole-with-legs-in-the-air-picture-id108000160'},
+        {id: 4, name: 'Tay', username: 'rickybobby31', bio: 'Dark secrets', email: 'tay@tay.com', avatar_url: 'https://pbs.twimg.com/profile_images/1264265275/Ricky_Bobby_closeup_400x400.jpg'}
       ]);
     });
 };
