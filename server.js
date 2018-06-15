08301b57c170c2a2f331722959923a4d39eb4308
talor-hammond <talorjhammond@gmail.com>
2018-06-15 12:05:19 +1200

    add static dir for express

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const hbs = require('express-handlebars')
 server.engine('hbs', hbs({extname: 'hbs'}))
 server.set('view engine', 'hbs')
 server.use(express.urlencoded({extended: true}))
+server.use(express.static('public'))
 
 server.use('/', router)
 
